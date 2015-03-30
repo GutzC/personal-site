4613fa732d0e8f5e234f520d7169208035f305dc
GutzC <camdengutzman@gmail.com>
2015-03-30 13:31:04 -0600

    Updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/Public"));
-
-app.listen(process.env.EXPRESS_PORT || 9090, function() {
+var port = process.env.EXPRESS_PORT || 9090;
+app.listen(port, function() {
     console.log("Listening to port " + port);
 });

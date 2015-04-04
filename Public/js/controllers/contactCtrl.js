cd699bf23dc0d74cc8fc796e3d396003693de86a
GutzC <camdengutzman@gmail.com>
2015-04-04 17:08:20 -0600

    Adding information to contact page. Restyled.

--- Public/js/controllers/contactCtrl.js
+++ Public/js/controllers/contactCtrl.js
@@ -2,4 +2,6 @@
 var app = angular.module("personal");
 app.controller("Contact", function(){
     this.test = "Welcome to the Contact page.";
+    this.phone_number = "801-427-9518";
+    this.display_phone = false;
 })
--- Public/js/controllers/projectsCtrl.js
+++ Public/js/controllers/projectsCtrl.js
@@ -8,17 +8,17 @@ app.controller("Projects", function(){
             screenshot: [{ image: "../../img/map-view.png", text: "Map View"}, {image: "../../img/list-view.png", text: "List View"}, { image: "../../img/list-view-add.png", text: "Add Billboard"}],
             repository: "https://github.com/GutzC/ldsbillboard",
             languages: ["HTML", "CSS", "Angular.JS", "Node/Express", "MongoDB"],
-            text: "Contracted by the LDS Church to create a simple billboard tracking app. Lead a team to create this app. Multiple views, using the Google Maps API"
+            text: "As our final group project at DevMountain, we contracted with the LDS Church to create a simple billboard tracking app. I lead a team of five to create this app, coordinating workflows. Multiple views, using the Google Maps API"
         },
         {
-            name: "SmitePantheon",
+            name: "SmitePantheon (in-progress)",
             screenshot: [{image: "../../img/sp-home.png", text: "Smite Pantheon"}, {image: "../../img/sp-gods.png", text: "God Information"}, { image: "../../img/sp-player-history.png", text: "Player History"}],
             repository: "https://github.com/GutzC/SmitePantheon",
             languages: ["HTML", "CSS", "Angular.JS", "Node/Express"],
             text: "A simple fan site for one of my favorite games, Smite. I created this project as a method to practice the languages I learned during my time at DevMountain. It is a project still in progress, but allowed me to play around with a couple interesting API's and CRUD calls."
         },
         {
-            name: "Rocket Math",
+            name: "Rocket Math (in-progress)",
             screenshot: [{ image: "../../img/rm-register.png", text: "Registration"}, { image: "../../img/rm-add.png", text: "Addition"}, { image: "../../img/rm-incorrect.png", text: "Incorrect Answer"}],
             repository: "https://github.com/GutzC/rocketmath",
             languages: ["HTML", "CSS", "Angular.JS", "Firebase"],

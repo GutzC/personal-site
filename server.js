a7521d4eb5a8b29d576145d63ee500c3e14b2dd7
GutzC <camdengutzman@gmail.com>
2015-04-04 19:54:54 -0600

    Updated descriptions

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
-var port = process.env.EXPRESS_PORT || 9090;
+//var port = process.env.EXPRESS_PORT || 9090;
+var port = 80;
 app.listen(port, function() {
     console.log("Listening to port " + port);
 });

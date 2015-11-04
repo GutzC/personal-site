d4db0aa8d88f1d4f6e5e95765074f2a5ad84a201
Camden Gutzman <camdengutzman@gmail.com>
2015-11-03 21:56:10 -0700

    Updating server port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 //var port = process.env.EXPRESS_PORT || 9090;
-var port = 9009;
+var port = 80;
 app.listen(port, function() {
     console.log("Listening to port " + port);
 });
